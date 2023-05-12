335efa842de582d579569ff91861d68fd4b06248
Dev205 <rodyael.205@gmail.com>
2023-05-12 10:58:58 +0300

    Begins code backEnd

---
+++ Calculator/Fonction.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Fonction
+    {
+        /// <summary>
+        /// Fonction qui place toujour le nombre zero sur l'ecran
+        /// </summary>
+        /// <param name="content"> strign source</param>
+        /// <param name="number"> nombre click</param>
+        /// <returns> string</returns>
+        public static string Zero(string content, string number)
+        {
+            if(content == "0")
+            {
+                if(number == "00")
+                {
+                    content = "0";
+                }
+                else
+                {
+                    content = "";
+                    content += number;
+                }
+            }
+            else
+            {
+                content += number;
+            }
+
+            return content;
+        }
+        /// <summary>
+        /// Fonction qui teste si l'ecran n'est pas vide
+        /// </summary>
+        /// <param name="content">chaine de caractere a tester</param>
+        /// <returns>valeur boolen</returns>
+        public static bool IsEmpty(string content)
+        {
+            try
+            {
+                double val = double.Parse(content);
+                if(content == string.Empty)
+                {
+                    return false;
+                }
+                
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Fonction qui teste si la valeur en entrer contien de chaine en entrer
+        /// </summary>
+        /// <param name="signes"> tableau de chaine</param>
+        /// <param name="content">contenue a tester</param>
+        /// <returns> boolen</returns>
+        public static bool IsOperatorMissigns(string[] signes, string content)
+        {
+            foreach(var signe in signes)
+            {
+                if (content.Contains(signe))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,9 +20,260 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Declaration des variables
+        private const int NBR_MAX_DISPLAY = 18;
+        private string[] signes = { "+", "-", "*", "/", "^" };
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // Fonction Evenement click
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            string val = "0";
+            if(displayBottom.Text.Length <= NBR_MAX_DISPLAY)
+            {
+                displayBottom.Text = Fonction.Zero(displayBottom.Text, val);
+                //displayBottom.Text += val; 
+            }
+        }
+
+        private void btn0Double_Click(object sender, RoutedEventArgs e)
+        {
+            string val = "00";
+            if (displayBottom.Text.Length <= NBR_MAX_DISPLAY)
+            {
+                //displayBottom.Text += val;
+                displayBottom.Text = Fonction.Zero(displayBottom.Text, val);
+            }
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            string val = "1";
+            if (displayBottom.Text.Length <= NBR_MAX_DISPLAY)
+            {
+
+                displayBottom.Text = Fonction.Zero(displayBottom.Text, val);//displayBottom.Text += val;
+            }
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            string val = "2";
+            if (displayBottom.Text.Length <= NBR_MAX_DISPLAY)
+            {
+                displayBottom.Text = Fonction.Zero(displayBottom.Text, val);//displayBottom.Text += val;
+            }
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            string val = "3";
+            if (displayBottom.Text.Length <= NBR_MAX_DISPLAY)
+            {
+                displayBottom.Text = Fonction.Zero(displayBottom.Text, val); //displayBottom.Text += val;
+            }
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            string val = "4";
+            if (displayBottom.Text.Length <= NBR_MAX_DISPLAY)
+            {
+                displayBottom.Text = Fonction.Zero(displayBottom.Text, val); //displayBottom.Text += val;
+            }
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            string val = "5";
+            if (displayBottom.Text.Length <= NBR_MAX_DISPLAY)
+            {
+                displayBottom.Text = Fonction.Zero(displayBottom.Text, val); //displayBottom.Text += val;
+            }
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            string val = "6";
+            if (displayBottom.Text.Length <= NBR_MAX_DISPLAY)
+            {
+                displayBottom.Text = Fonction.Zero(displayBottom.Text, val); //displayBottom.Text += val;
+            }
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            string val = "7";
+            if (displayBottom.Text.Length <= NBR_MAX_DISPLAY)
+            {
+                displayBottom.Text = Fonction.Zero(displayBottom.Text, val); //displayBottom.Text += val;
+            }
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            string val = "8";
+            if (displayBottom.Text.Length <= NBR_MAX_DISPLAY)
+            {
+                displayBottom.Text = Fonction.Zero(displayBottom.Text, val); //displayBottom.Text += val;
+            }
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            string val = "9";
+            if (displayBottom.Text.Length <= NBR_MAX_DISPLAY)
+            {
+                displayBottom.Text = Fonction.Zero(displayBottom.Text, val); //displayBottom.Text += val;
+            }
+        }
+
+        private void btnVirgule_Click(object sender, RoutedEventArgs e)
+        {
+            string val = ",";
+            if (displayBottom.Text.Length <= NBR_MAX_DISPLAY)
+            {
+                if (!displayBottom.Text.Contains(","))
+                {
+                    displayBottom.Text += val;
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Supprime de dernier element mais si il reste 1 element on remplace par 0
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnBackspace_Click(object sender, RoutedEventArgs e)
+        {
+            int index = displayBottom.Text.Length;
+            if(index > 1)
+            {
+                displayBottom.Text = displayBottom.Text.Remove(index - 1, 1);
+
+            }
+            else
+            {
+                displayBottom.Text = "0";
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            displayBottom.Text = "0";
+        }
+
+        private void btnClearAll_Click(object sender, RoutedEventArgs e)
+        {
+            displayBottom.Text = "0";
+            displayTop.Text = "";
+        }
+
+        private void btnPourcentage_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                displayTop.Text = displayBottom.Text + "/100";
+                float percnet = float.Parse(displayBottom.Text);
+                displayBottom.Text = (percnet / 100).ToString();
+            }
+            catch
+            {
+                displayBottom.Text = "SYNTAX ERROR";
+
+            }
+        }
+
+        private void btnXCarre_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                displayTop.Text = "²(" + displayBottom.Text + ")";
+                float percnet = float.Parse(displayBottom.Text);
+                displayBottom.Text = (percnet * percnet).ToString();
+            }
+            catch
+            {
+                displayBottom.Text = "SYNTAX ERROR";
+
+            }
+        }
+
+        private void btnRacinecarre_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                displayTop.Text = "(" + displayBottom.Text + ")Sqrt";
+                double percnet = double.Parse(displayBottom.Text);
+                displayBottom.Text = (Math.Sqrt(percnet)).ToString();
+            }
+            catch
+            {
+                displayBottom.Text = "SYNTAX ERROR";
+
+            }
+        }
+
+        // OPERATION + - / * = *********************************************S
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            if (Fonction.IsEmpty(displayBottom.Text))
+            {
+                if(Fonction.IsOperatorMissigns(signes, displayBottom.Text))
+                {
+                    displayBottom.Text += "+";
+                }
+            }
+        }
+
+        private void btnMoins_Click(object sender, RoutedEventArgs e)
+        {
+            if (Fonction.IsEmpty(displayBottom.Text))
+            {
+                if (Fonction.IsOperatorMissigns(signes, displayBottom.Text))
+                {
+                    displayBottom.Text += "-";
+                }
+            }
+        }
+
+        private void btnMultiple_Click(object sender, RoutedEventArgs e)
+        {
+            if (Fonction.IsEmpty(displayBottom.Text))
+            {
+                if (Fonction.IsOperatorMissigns(signes, displayBottom.Text))
+                {
+                    displayBottom.Text += "*";
+                }
+            }
+        }
+
+        private void btnDivision_Click(object sender, RoutedEventArgs e)
+        {
+            if (Fonction.IsEmpty(displayBottom.Text))
+            {
+                if (Fonction.IsOperatorMissigns(signes, displayBottom.Text))
+                {
+                    displayBottom.Text += "/";
+                }
+            }
+        }
+
+        private void btnCarre_Click(object sender, RoutedEventArgs e)
+        {
+            if (Fonction.IsEmpty(displayBottom.Text))
+            {
+                if (Fonction.IsOperatorMissigns(signes, displayBottom.Text))
+                {
+                    displayBottom.Text += "^";
+                }
+            }
         }
     }
 }
